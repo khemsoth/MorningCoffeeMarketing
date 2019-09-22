85d9d4d8c54c2b1ce32089083373802e91fbde39
Keith Hemsoth <kevhemsoth@gmail.com>
2019-09-22 12:02:14 -0400

    Added grid layout

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 const PORT = process.env.PORT || 3000;
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use('/', router);
+app.use(express.static(__dirname + '/assets'))
 
 app.listen(PORT, function(req, res) {
   console.log('App listening on port: ' + PORT)
