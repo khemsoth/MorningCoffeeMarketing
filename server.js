e7379bd5d53d7c8b9b6d86becef156a601f629d2
unknown <kevhemsoth@gmail.com>
2019-09-30 13:14:15 -0400

    contact form works (spinwheel present)

--- server.js
+++ server.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const router = express.Router();
 const PORT = process.env.PORT || 3000;
-const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const GMAIL_USER = process.env.GMAIL_USER;
+const GMAIL_PASS = process.env.GMAIL_PASS;
 
-dotenv.config();
+require('dotenv').config();
 
+app.use(bodyParser.urlencoded({extended: true}))
+
+// ROUTES
 router.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+app.post('/contact', function(req, res) {
+  const smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com', 
+    port: 465, 
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  })
+  const mailOpts = {
+    from: 'Your sender info',
+    to: process.env.GMAIL_USER,
+    subject: 'New message from MCM site contact form', 
+    text: `${req.body.name} (${req.body.email}) 
+    subject:
+      ${req.body.subject} 
+    message: 
+      ${req.body.message}`
+  }
+  smtpTrans.sendMail(mailOpts, function(err, res) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+  })
+})
 
 app.use('/', router);
 app.use(express.static(__dirname + '/assets'))
 
 app.listen(PORT, function(req, res) {
   console.log('App listening on port: ' + PORT)
-})
+});
+
+console.log(GMAIL_USER);
+console.log(process.env.GMAIL_USER);
